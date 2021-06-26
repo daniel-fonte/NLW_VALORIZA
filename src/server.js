03d64eebe7d1b545e096aa67a267bdb55a76b7fa
Daniel Nunes da Fonte <danielnunesdafonte9@gmail.com>
2021-06-26 18:31:29 -0300

    Added initial template project

---
+++ src/server.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+var PORT = 3333;
+app.listen(PORT, function () { return console.log("Server started on port " + PORT); });
